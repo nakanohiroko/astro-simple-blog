ed4f3b1e0066689fc1968eeb417d80df4a6e05c3
nakanohiroko <piroconbu@gmail.com>
2025-04-02 14:11:53 +0900

    [fix] Vercelのパッケージを削除

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel";
 
 export default defineConfig({
   site: "https://astro-simple-blog-black.vercel.app",
   output: "static",
   integrations: [tailwind()],
-  adapter: vercel(),
 });
