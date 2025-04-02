17ddbf9482ba66eb5aee4137c7cc8ede2895a700
nakanohiroko <piroconbu@gmail.com>
2025-04-02 15:23:33 +0900

    [add] @astrojs/cloudflareを追加

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import cloudflare from "@astrojs/cloudflare";
 
 export default defineConfig({
   site: "https://astro-simple-blog-black.vercel.app",
   integrations: [tailwind()],
+  adapter: cloudflare(),
 });
