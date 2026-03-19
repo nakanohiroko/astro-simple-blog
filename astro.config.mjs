d57ebfb9b8443ec0cdc0088e4a6e956b03364f61
nakanohiroko <piroconbu@gmail.com>
2026-03-20 00:28:16 +0900

    feature: Astro6.0にアップグレード

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  site: "https://astro-simple-blog-omega.vercel.app/",
-  integrations: [tailwind()],
+  site: "http://localhost:4321/",
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
