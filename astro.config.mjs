b6c6152ef723d0719f152a3f5ea412cf95a1d2ba
nakanohiroko <piroconbu@gmail.com>
2026-03-20 00:49:38 +0900

    fix: @astrojs/vercelを削除

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,9 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
-import vercel from "@astrojs/vercel";
 
 export default defineConfig({
   site: "http://localhost:4321/",
   vite: {
     plugins: [tailwindcss()],
   },
-  adapter: vercel(),
 });
